a24f6f3f0567d5b053f3004f5ffb1a7eada84922
Yu Chen <intellectual.y@gmail.com>
2018-05-14 16:55:32 +0800

    add dependency check for GeneExtractor

--- src/main/java/com/spdeveloper/chgc/genome/prediction/service/GeneExtractor.java
+++ src/main/java/com/spdeveloper/chgc/genome/prediction/service/GeneExtractor.java
@@ -19,42 +19,57 @@ import org.springframework.stereotype.Service;
 
 import com.spdeveloper.chgc.genome.prediction.entity.GenePrediction;
 import com.spdeveloper.chgc.genome.util.cmd.IntegratedProgram;
+import com.spdeveloper.chgc.genome.util.debug.ComparisonUtil;
 import com.spdeveloper.chgc.genome.util.file.WriteToFileUtil;
 
 @Service
 public class GeneExtractor {
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Value("${cmdTemplate.extractor}")
 	String cmdTemplate;
-	
+
 	@Autowired
 	GenePredictionResultCombiner genePredictionResultCombiner;
-	
+
 	@PostConstruct
 	public void dependencyCheck() throws IOException, InterruptedException {
-		File fasta = Paths.get("src", "main", "resources", "files", "dependencyCheck", "short.fas").toFile();
-		Path tempDir = Files.createTempDirectory("genomeAnalysis");
-		List<GenePrediction> predictions = genePredictionResultCombiner.combine(fasta, tempDir);
-		extract(fasta, predictions, tempDir);
-		log.info("Delete tempDir: " + tempDir.toFile().getAbsolutePath());
-		FileUtils.deleteDirectory(tempDir.toFile());
-
+		try {
+			File fasta = Paths.get("src", "main", "resources", "files", "dependencyCheck", "short.fas").toFile();
+			File predictions = Paths.get("src", "main", "resources", "files", "dependencyCheck", "Gene.xlsx").toFile();
+			Path tempDir = Files.createTempDirectory("genomeAnalysis");
+			Path actual = extract(fasta, predictions, tempDir).toPath();
+			Path expected = Paths.get("src", "main", "resources", "files", "dependencyCheck", "gene.fas");
+			String comparisonReport = ComparisonUtil.diffs(Files.readAllLines(expected).toArray(new String[0]), Files.readAllLines(actual).toArray(new String[0]));
+			if(comparisonReport.contains("identical")) {
+				
+			}else {
+				throw new MissDependencyException(comparisonReport);
+			}
+			log.info("Delete tempDir: " + tempDir.toFile().getAbsolutePath());
+			FileUtils.deleteDirectory(tempDir.toFile());
+		} catch (Exception e) {
+			throw new MissDependencyException("Glimmer is not working.", e);
+		}
 	}
-	
-	public File extract(File fastaFile, List<GenePrediction> predictions, Path tempDir) throws IOException, InterruptedException {
-		Path predictionsFile = writeToFile(predictions);
+
+	public File extract(File fastaFile, List<GenePrediction> predictions, Path tempDir)
+			throws IOException, InterruptedException {
+		Path predictionsFile = writeToFile(predictions, tempDir);
 		return extract(fastaFile, predictionsFile.toFile(), tempDir);
 	}
+
 	public File extract(File fastaFile, File predictions, Path tempDir) throws IOException, InterruptedException {
 		IntegratedProgram extractor = new IntegratedProgram(cmdTemplate, null);
 		Path extractorTempFile = Files.createTempFile(tempDir, "genomeAnalysis", "gene.fas");
-		extractor.execute(null, extractorTempFile, fastaFile.getAbsolutePath(),predictions.getAbsolutePath(), extractorTempFile.toFile().getAbsolutePath());
+		extractor.execute(null, extractorTempFile, fastaFile.getAbsolutePath(), predictions.getAbsolutePath(),
+				extractorTempFile.toFile().getAbsolutePath());
 		return extractorTempFile.toFile();
 	}
-	private Path writeToFile(List<GenePrediction> predictions) throws IOException {
-		Path path = Paths.get("/","root","gene.xlsx");
+
+	private Path writeToFile(List<GenePrediction> predictions, Path tempDir) throws IOException {
+		Path path = Files.createTempFile(tempDir, "genomeAnalysis", null);
 		WriteToFileUtil.writeToFile(predictions, path);
 		return path;
 	}
