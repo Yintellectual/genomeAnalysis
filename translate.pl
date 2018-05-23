08b6182cf0e1cb7a8203364e403fc67f31f3e397
Yu Chen <intellectual.y@gmail.com>
2018-05-23 11:43:03 +0800

    update extractor and translate scripts

---
+++ translate.pl
@@ -0,0 +1,103 @@
+#!/usr/bin/perl -w
+use strict;
+#use warnings;
+die "Usage: perl $0 ORF > protein\n" unless (@ARGV == 1);
+open(IN,$ARGV[0]) or die "Can't open $ARGV[0]\n";
+
+local $/ = '>';
+while(<IN>){
+        chomp;
+        my ($name, $sequence) = split(/\n/,$_,2);
+        next unless ($sequence && $name);
+
+        $sequence =~ s/\s+//g;
+        my $protein = "";
+
+        for(my $i=0; $i < (length($sequence) - 2) ; $i += 3) {
+    my $codon = substr($sequence, $i, 3);
+    last if( length( $codon ) < 3 );
+    $protein .= codon2aa( $codon );
+  }
+
+  print ">$name\n$protein\n";
+}
+close(IN);
+
+
+sub codon2aa {
+    my($codon) = @_;
+    $codon = uc $codon;
+    my(%genetic_code) = (
+
+    'TCA' => 'S',    # Serine
+    'TCC' => 'S',    # Serine
+    'TCG' => 'S',    # Serine
+    'TCT' => 'S',    # Serine
+    'TTC' => 'F',    # Phenylalanine
+    'TTT' => 'F',    # Phenylalanine
+    'TTA' => 'L',    # Leucine
+    'TTG' => 'L',    # Leucine
+    'TAC' => 'Y',    # Tyrosine
+    'TAT' => 'Y',    # Tyrosine
+    'TAA' => '*',    # Stop
+    'TAG' => '*',    # Stop
+    'TGC' => 'C',    # Cysteine
+    'TGT' => 'C',    # Cysteine
+    'TGA' => '*',    # Stop
+    'TGG' => 'W',    # Tryptophan
+    'CTA' => 'L',    # Leucine
+    'CTC' => 'L',    # Leucine
+    'CTG' => 'L',    # Leucine
+    'CTT' => 'L',    # Leucine
+    'CCA' => 'P',    # Proline
+    'CCC' => 'P',    # Proline
+    'CCG' => 'P',    # Proline
+    'CCT' => 'P',    # Proline
+    'CAC' => 'H',    # Histidine
+    'CAT' => 'H',    # Histidine
+    'CAA' => 'Q',    # Glutamine
+    'CAG' => 'Q',    # Glutamine
+    'CGA' => 'R',    # Arginine
+    'CGC' => 'R',    # Arginine
+    'CGG' => 'R',    # Arginine
+    'CGT' => 'R',    # Arginine
+    'ATA' => 'I',    # Isoleucine
+    'ATC' => 'I',    # Isoleucine
+    'ATT' => 'I',    # Isoleucine
+    'ATG' => 'M',    # Methionine
+    'ACA' => 'T',    # Threonine
+    'ACC' => 'T',    # Threonine
+    'ACG' => 'T',    # Threonine
+    'ACT' => 'T',    # Threonine
+    'AAC' => 'N',    # Asparagine
+    'AAT' => 'N',    # Asparagine
+    'AAA' => 'K',    # Lysine
+    'AAG' => 'K',    # Lysine
+    'AGC' => 'S',    # Serine
+    'AGT' => 'S',    # Serine
+    'AGA' => 'R',    # Arginine
+    'AGG' => 'R',    # Arginine
+    'GTA' => 'V',    # Valine
+    'GTC' => 'V',    # Valine
+    'GTG' => 'V',    # Valine
+    'GTT' => 'V',    # Valine
+    'GCA' => 'A',    # Alanine
+    'GCC' => 'A',    # Alanine
+    'GCG' => 'A',    # Alanine
+    'GCT' => 'A',    # Alanine
+    'GAC' => 'D',    # Aspartic Acid
+    'GAT' => 'D',    # Aspartic Acid
+    'GAA' => 'E',    # Glutamic Acid
+    'GAG' => 'E',    # Glutamic Acid
+    'GGA' => 'G',    # Glycine
+    'GGC' => 'G',    # Glycine
+    'GGG' => 'G',    # Glycine
+    'GGT' => 'G',    # Glycine
+    );
+
+    if(exists $genetic_code{$codon}) {
+      return $genetic_code{$codon};
+    }else{
+                        return '*';
+    }
+}
